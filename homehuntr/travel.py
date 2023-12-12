5e28487745b1c9d97c46fee0a45f6a960d13525d
jeffbrennan <jeffbrennan10@gmail.com>
2023-12-11 21:03:40 -0500

    search by place id instead of address, generalize for usage in main.py

--- homehuntr/travel.py
+++ homehuntr/travel.py
@@ -4,7 +4,12 @@ from dotenv import load_dotenv
 import json
 from pathlib import Path
 import argparse
-from typing import Optional
+from typing import Optional, TypedDict
+
+
+class Place(TypedDict):
+    place_id: str
+    address: str
 
 
 def get_place_id(address: str) -> Optional[str]:
@@ -32,42 +37,74 @@ def get_place_id(address: str) -> Optional[str]:
     return response.json()["candidates"][0]["place_id"]
 
 
+def request_directions(origin: Place, destination: Place, mode: str) -> None:
     load_dotenv()
     MAPS_API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
     request_url = "https://maps.googleapis.com/maps/api/directions/json"
 
     params = {
-        "destination": destination,
+        "destination": destination["place_id"],
         "mode": mode,
-        "origin": origin,
+        "origin": origin["place_id"],
         "key": MAPS_API_KEY,
     }
+    print(
+        f"[{origin['address']}] [{mode}] - Requesting directions to {destination['address']}"
+    )
     response = requests.get(request_url, params=params)
 
-    if response.status_code == 200:
-        dump_directions(response.json(), mode)
+    if response.status_code == 200 and response.json()["status"] == "OK":
+        dump_directions(response.json(), origin, destination, mode)
 
 
-def dump_directions(directions: dict, mode) -> None:
-    origin_uid = directions["geocoded_waypoints"][0]["place_id"]
-    destination_uid = directions["geocoded_waypoints"][1]["place_id"]
-    file_name = f"{origin_uid}_{destination_uid}_{mode}"
+def dump_directions(
+    directions: dict, origin: Place, destination: Place, mode: str
+) -> None:
+    file_name = f"{origin['place_id']}_{destination['place_id']}_{mode}"
     output_path = Path(__file__).parent / "data" / "directions" / f"{file_name}.json"
     with open(output_path, "w") as f:
         json.dump(directions, f)
+
+
+def get_directions(address: Optional[str] = None, uid: Optional[str] = None) -> None:
+    if address is None and uid is None:
+        raise ValueError("Must provide either origin or uid")
+
+    if address is None:
+        with open(Path(__file__).parent / "data" / "address" / f"{uid}.json") as f:
+            address_data = json.load(f)
+        address = address_data["building_address"]
+
+    if address is None:
+        raise ValueError("Could not resolve address")
+
+    if uid is None:
+        uid = get_place_id(address)
+
+    if uid is None:
+        raise ValueError("Could not resolve uid")
+
+    origin: Place = {"place_id": uid, "address": address}
+
+    with open(
+        Path(__file__).parent / "data" / "destinations" / "destinations.json"
+    ) as f:
+        destination_data = json.load(f)
+
+    destinations: list[Place] = [
+        {"place_id": x["place_id"], "address": x["address"]} for x in destination_data
+    ]
+
+    for destination in destinations:
+        request_directions(origin, destination, "transit")
+        request_directions(origin, destination, "bicycling")
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("origin", help="The origin address")
     args = parser.parse_args()
-
-    with open(Path(__file__).parent / "data" / "destinations.json") as f:
-        destinations = json.load(f)
-
-    for destination in destinations:
-        get_directions(args.origin, destination, "transit")
-        get_directions(args.origin, destination, "bicycling")
+    get_directions(address=args.origin)
 
 
 if __name__ == "__main__":
