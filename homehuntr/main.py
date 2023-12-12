528a8a052d7a7fdbdbfd58739faf130346de6945
jeffbrennan <jeffbrennan10@gmail.com>
2023-12-11 22:49:43 -0500

    add main pipeline

---
+++ homehuntr/main.py
@@ -0,0 +1,26 @@
+import argparse
+from scrape_streeteasy import scrape_apartment_url
+from travel import get_directions
+from compute_distance import parse_distance
+
+# from clean_address import clean_address
+# from create_obt import build_obt
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--url", help="url to scrape", required=True)
+    args = parser.parse_args()
+
+    scraping_result = scrape_apartment_url(args.url)
+    get_directions(uid=scraping_result["uid"])
+
+    parse_distance(run_type="overwrite")
+
+    # parse_address(address_uid)
+
+    # build_obt()
+
+
+if __name__ == "__main__":
+    main()
