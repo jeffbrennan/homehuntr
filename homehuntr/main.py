67f4d91ab8d82a846fbdc5b3293fc85c0562a668
jeffbrennan <jeffbrennan10@gmail.com>
2023-12-13 00:12:35 -0500

    Add dedupe_directions function

--- homehuntr/main.py
+++ homehuntr/main.py
@@ -2,6 +2,7 @@ import argparse
 from scrape_streeteasy import scrape_apartment_url
 from travel import get_directions
 from compute_distance import parse_distance
+from clean_delta import dedupe_directions
 
 # from clean_address import clean_address
 # from create_obt import build_obt
@@ -16,6 +17,7 @@ def main():
     get_directions(uid=scraping_result["uid"])
 
     parse_distance(run_type="overwrite")
+    dedupe_directions()
 
     # parse_address(address_uid)
 
