80383dda32265f74539f69c6704e0e5cc384a556
jeffbrennan <jeffbrennan10@gmail.com>
2024-01-12 01:37:40 -0500

    rewrite to use polars & delta-rs instead of spark

--- homehuntr/clean_delta.py
+++ homehuntr/clean_delta.py
@@ -1,44 +1,41 @@
-import pyspark.sql.functions as F
-from pyspark.sql import SparkSession
-from common import get_spark
-from delta import DeltaTable
+from homehuntr import common
+from deltalake import DeltaTable
+import polars as pl
 
 
-def dedupe_delta(
-    spark: SparkSession, delta_path: str, partition_cols: list, order_cols: list
-):
+def dedupe_delta(delta_path: str, partition_cols: list, order_cols: list):
     """
     Utility to remove duplicate rows from a delta table
     """
-    df = spark.read.format("delta").load(delta_path)
-    df.createOrReplaceTempView("df")
 
-    partition_str = ", ".join(partition_cols)
-    order_str = ", ".join(order_cols)
+    _, token = common.get_gcp_fs()
+    df = pl.read_delta(delta_path, storage_options={"SERVICE_ACCOUNT": token})
+
+    if not isinstance(df, pl.DataFrame):
+        raise ValueError("Delta table not found")
+
+    if len(order_cols) > 1:
+        raise ValueError("Only one order column is currently supported")
 
     df_no_dupes = (
-        spark.sql(
-            f"""
-                SELECT *,
-                ROW_NUMBER() OVER (PARTITION BY {partition_str} ORDER BY {order_str}) rn
-                FROM df
-                """
+        df.with_columns(
+            rn=pl.col(order_cols).rank("ordinal", descending=True).over(partition_cols)
         )
-        .filter(F.col("rn") == 1)
+        .filter(pl.col("rn") == 1)
         .drop("rn")
     )
 
-    df_no_dupes.write.format("delta").mode("overwrite").save(delta_path)
+    df_no_dupes.write_delta(
+        delta_path, mode="overwrite", storage_options={"SERVICE_ACCOUNT": token}
+    )
 
-    delta_table = DeltaTable.forPath(spark, delta_path)
+    delta_table = DeltaTable(delta_path, storage_options={"SERVICE_ACCOUNT": token})
     delta_table.optimize()
-    delta_table.vacuum(0)
+    delta_table.vacuum(retention_hours=1, enforce_retention_duration=False)
 
 
 def dedupe_directions():
-    spark = get_spark()
     dedupe_delta(
-        spark=spark,
         delta_path="gs://homehuntr-storage/delta/transit_directions",
         partition_cols=["origin_id", "destination_id"],
         order_cols=["transit_min"],
